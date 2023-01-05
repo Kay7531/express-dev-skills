d5bab1126a372458db85bb11b52e5af670443ffe
User Name <kauth333@gmail.com>
2023-01-05 18:00:34 -0500

    refactor skills router in server.js and routes/skills.js

---
+++ data/skill-data.js
@@ -0,0 +1,11 @@
+const skills = [
+   {text:'Numchucking', aquired: true, _id: 345641 }, 
+   {text:'Shuriken Throwing', aquired: false, _id: 346475}, 
+   {text:'Campaign Managing', aquired: true, _id: 357612 }, 
+   {text:'Dancing', aquired: true, _id: 358908 },
+   {text:'Liger Drawing', aquired: false, _id: 367513} 
+]
+
+export{
+    skills
+}
