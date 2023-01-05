6613806e581c95bb9503bfa39cf51f8ffc224da3
User Name <kauth333@gmail.com>
2023-01-05 18:32:34 -0500

    Add controller directory and import skills dataset and add index function  in controller/skills.js. Also refactored route/skills.js so it no longer imports dataset and remove index function and add skillCtrl callback

--- routes/skills.js
+++ routes/skills.js
@@ -1,15 +1,14 @@
 import { Router } from 'express'
-//import skills data
-import { skills } from '../data/skill-data.js'
+
+import * as skillsCtrl from '../controllers/skills.js'
 
 const router = Router()
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-  res.render('skills/index',{
-    skills: skills
-  })
-})
+router.get('/', skillsCtrl.index)
+
+
+
 
 export {
   router
