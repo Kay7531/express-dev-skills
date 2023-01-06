ccae1c52375a194539b246c6c1b29ab2b926e978
User Name <kauth333@gmail.com>
2023-01-05 23:35:09 -0500

    add router.post in router module that callsback skillsCtrl.create controller action added in controller/skill.js file.

--- controllers/skills.js
+++ controllers/skills.js
@@ -17,8 +17,23 @@ function index(req, res) {
 function newSkill(req, res) {
     res.render('skills/new')
 }
+
+function create(req, res) {
+    console.log(req.body)
+    req.body.acquired = false
+    Skill.create(req.body)
+    .then(skill => {
+          // Notice we are doing a redirect here!
+      res.redirect('/skills')
+    })
+    .catch(error => {
+      console.log(error)
+      res.redirect('/skills')
+    })
+  }
   
   export {
       index,
       newSkill as new,
+      create,
   }
